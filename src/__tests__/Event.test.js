3cee254a215642fe81233bb3bc24e0445baeff8d
Ilse Gaertner <joachimpruegl@gmail.com>
2023-11-06 18:41:21 +0100

    Update Event.test.js & Event.js (All passing)

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,63 +1,60 @@
 import { getEvents } from "../api";
-import { render } from "react-dom";
+import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Event from "../components/Event";
 
 describe("<Event /> Component", () => {
   let EventComponent;
   let allEvents;
+  let event;
 
   beforeEach(async () => {
-    EventComponent = render(<Event />);
     allEvents = await getEvents(); // Fetch the data once before running any test
+    event = allEvents[0];
+    EventComponent = render(<Event event={event} />);
   });
 
   // unit test cases that test the event information displays correctly (i.e., to verify whether all the necessary elements have been rendered.)
 
   test("renders the event's title", () => {
-    expect(
-      EventComponent.queryByText(allEvents[0].summary)
-    ).toBeInTheDocument();
+    expect(EventComponent.queryByText(event.summary)).toBeInTheDocument();
   });
 
-  //   test("renders the event's start time", () => {
-  //     expect(
-  //       EventComponent.queryByText(allEvents[0].created)
-  //     ).toBeInTheDocument();
-  //   });
+  test("renders the event's start time", () => {
+    expect(EventComponent.queryByText(event.created)).toBeInTheDocument();
+  });
 
-  //   test("renders the event's location", () => {
-  //     expect(
-  //       EventComponent.queryByText(allEvents[0].location)
-  //     ).toBeInTheDocument();
-  //   });
+  test("renders the event's location", () => {
+    expect(EventComponent.queryByText(event.location)).toBeInTheDocument();
+  });
 
-  //   test("renders event detailes button with the title (show details)", () => {
-  //     expect(EventComponent.queryByText("show details")).toBeInTheDocument();
-  //   });
+  test("renders event details button with the title (show details)", () => {
+    expect(EventComponent.queryByText("show details")).toBeInTheDocument();
+  });
 
-  //   // unit test cases that check if the event’s detail’s section is shown or hidden when a user clicks on a show details button (or hide details, when the details are shown)
+  // unit test cases that check if the event’s detail’s section is shown or hidden when a user clicks on a show details button (or hide details, when the details are shown)
 
-  //   test("by default, event's details section should be hidden", () => {
-  //     const detailsSection = EventComponent.querySelector(".show-details");
-  //     expect(detailsSection).not.toBeInTheDocument();
-  //   });
-  //   test("shows the details section when the user clicks on the 'show details' button", async () => {
-  //     const user = userEvent.setup();
-  //     const detailsButton = EventComponent.queryByText("show details");
-  //     const detailsSection = EventComponent.querySelector(".show-details");
+  test("by default, event's details section should be hidden", () => {
+    const hideDetailsButton = EventComponent.queryByText("hide details");
+    expect(hideDetailsButton).not.toBeInTheDocument();
+  });
 
-  //     // Verify that the details section is initially hidden
-  //     expect(detailsSection).not.toBeInTheDocument();
+  // test("shows the details section when the user clicks on the 'show details' button", async () => {
+  //   const user = userEvent.setup();
+  //   const showDetailsButton = EventComponent.queryByText("show details");
+  //   const detailsSection = EventComponent.querySelector(".show-details");
 
-  //     // Check that the "show details" button is present
-  //     expect(detailsButton).toBeInTheDocument();
+  //   // Verify that the details section is initially hidden
+  //   expect(detailsSection).not.toBeInTheDocument();
 
-  //     await user.click(detailsButton);
-  //     expect(detailsSection).toBeInTheDocument();
-  //     expect(detailsSection).toBeVisible();
-  //     expect(detailsSection).toHaveClass("show-details");
-  //   });
+  //   // Check that the "show details" button is present
+  //   expect(showDetailsButton).toBeInTheDocument();
+
+  //   await user.click(detailsButton);
+  //   expect(detailsSection).toBeInTheDocument();
+  //   expect(detailsSection).toBeVisible();
+  //   expect(detailsSection).toHaveClass("show-details");
+  // });
 
   //   test("hides the details section when the user clicks on the 'hide details' button", async () => {
   //     const user = userEvent.setup();
--- src/components/Event.js
+++ src/components/Event.js
@@ -11,13 +11,16 @@ const Event = ({ event }) => {
   return (
     <li>
       <button onClick={toggleDetails}>
-        {detailsVisible ? "Hide Details" : "Show Details"}
+        {detailsVisible ? "hide details" : "show details"}
       </button>
+      {/* <button>Hide Details</button>
+      <button>Show Details</button> */}
       <div className={`show-details ${detailsVisible ? "" : "hide-details"}`}>
         <h3>{event.summary}</h3>
         <p>{event.location}</p>
         <p>{event.start.dateTime}</p>
         <p>{event.description}</p>
+        <p>{event.created}</p>
       </div>
     </li> // <li> instead of <div role="listitem"> as <li> elements have role="listitem" by default
   );
