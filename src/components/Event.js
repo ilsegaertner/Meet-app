7722d7b64a8267d81f4710f7ba2afda7abe29f29
ilsegaertner <joachimpruegl@gmail.com>
2023-11-06 16:01:53 +0100

    add event.test.js and event.js

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,25 @@
-const Event = () => {
+import React from "react";
+import { useState } from "react";
+
+const Event = ({ event }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+
+  const toggleDetails = () => {
+    setDetailsVisible(!detailsVisible);
+  };
+
   return (
-    <li></li> // <li> instead of <div role="listitem"> as <li> elements have role="listitem" by default
+    <li>
+      <button onClick={toggleDetails}>
+        {detailsVisible ? "Hide Details" : "Show Details"}
+      </button>
+      <div className={`show-details ${detailsVisible ? "" : "hide-details"}`}>
+        <h3>{event.summary}</h3>
+        <p>{event.location}</p>
+        <p>{event.start}</p>
+        <p>{event.description}</p>
+      </div>
+    </li> // <li> instead of <div role="listitem"> as <li> elements have role="listitem" by default
   );
 };
 
