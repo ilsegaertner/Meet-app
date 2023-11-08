cb45550711fa791f703f09d8fd2289335b264a4a
Ilse Gaertner <joachimpruegl@gmail.com>
2023-11-08 13:54:49 +0100

    Update NumberOfEvent.test with new test

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,6 +1,6 @@
 // import { getEvents } from "../api";
 import { render } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import NumberOfEvents from "../components/NumberOfEvents";
 
 describe("<NumberOfEvents /> Component", () => {
@@ -17,6 +17,29 @@ describe("<NumberOfEvents /> Component", () => {
 
   test("renders 32 as default value", () => {
     const numbersInput = NumberOfEventsComponent.queryByRole("textbox");
+    expect(numbersInput.value).toBe("32");
+  });
+
+  test("updates the textboxes value when a user types in it", async () => {
+    const user = userEvent.setup();
+    const numbersInput = NumberOfEventsComponent.queryByRole("textbox");
+
+    // Clear the input before typing
+    user.clear(numbersInput);
+
+    // Simulate the user typing a value
+    // await user.type(numbersInput, "42");
+    await user.type(numbersInput, "{backspace}{backspace}10");
+
+    // Check if the input's value reflects the change
+    expect(numbersInput.value).toBe("10");
+  });
+
+  test("prevents entering non-numeric numbers", () => {
+    const user = userEvent.setup();
+    const numbersInput = NumberOfEventsComponent.queryByRole("textbox");
+    user.click(numbersInput);
+    user.type(numbersInput, "abc123");
     expect(numbersInput.value).toBe("32");
   });
 });
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -3,12 +3,7 @@ import React from "react";
 const NumberOfEvents = () => {
   return (
     <div id="number-of-events">
-      <input
-        type="number"
-        className="event-numbers"
-        defaultValue={32}
-        role="textbox"
-      />
+      <input type="textbox" className="event-numbers" defaultValue={32} />
     </div>
   );
 };
