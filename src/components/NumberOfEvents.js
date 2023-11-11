b5a7d829e0744bee90aec216c3c364ffe75d105a
Ilse Gaertner <joachimpruegl@gmail.com>
2023-11-11 19:11:39 +0100

    change label of numberOfEvents

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -11,7 +11,7 @@ const NumberOfEvents = ({ setCurrentNOE }) => {
 
   return (
     <div id="number-of-events">
-      <label htmlFor="event-numbers">Number of Events: </label>
+      <label htmlFor="number-of-events">Number of Events: </label>
       <input
         type="textbox"
         className="event-numbers"
