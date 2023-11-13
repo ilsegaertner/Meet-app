cbeb36e8707b01bf449153039db1f61ee52b3b99
Ilse Gaertner <joachimpruegl@gmail.com>
2023-11-13 08:03:02 +0100

    Delete Comments in Event Component

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,12 +2,6 @@ import React from "react";
 import { useState } from "react";
 
 const Event = ({ event }) => {
-  // const [detailsVisible, setDetailsVisible] = useState(false);
-
-  // const toggleDetails = () => {
-  //   setDetailsVisible(!detailsVisible);
-  // };
-
   const [showDetails, setShowDetails] = useState(false);
 
   return (
@@ -33,26 +27,5 @@ const Event = ({ event }) => {
     </li>
   );
 };
-
-//      <div>
-//      <button onClick={toggleDetails}>
-//       {detailsVisible ? "Hide Details" : "Show Details"}
-//     </button>
-//   {/* <button>Hide Details</button>
-//       <button>Show Details</button> */}
-//     <li
-//         className={`show-details ${
-//           detailsVisible ? "details-shown" : "details-hidden"
-//         }`}
-//       >
-//         <h3>{event.summary}</h3>
-//         <p>{event.location}</p>
-//         <p>{event.start.dateTime}</p>
-//         <p>{event.description}</p>
-//         <p>{event.created}</p>
-//       </li>
-//     </div>
-//   );
-// };
 
 export default Event;
