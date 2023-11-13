f661dd2fd69f2ee585ff1e7b903eece8339db507
Ilse Gaertner <joachimpruegl@gmail.com>
2023-11-13 08:02:46 +0100

    Refactor Event component and toggleButton / Add tests for refactored Event Component

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -20,8 +20,8 @@ describe("<Event /> Component", () => {
     expect(EventComponent.queryByText(event.summary)).toBeInTheDocument();
   });
 
-  test("renders the event's start time", () => {
-    expect(EventComponent.queryByText(event.created)).toBeInTheDocument();
+  test("doesn't render the 'event created' node by default", () => {
+    expect(EventComponent.queryByText(event.created)).not.toBeInTheDocument();
   });
 
   test("renders the event's location", () => {
@@ -29,40 +29,35 @@ describe("<Event /> Component", () => {
   });
 
   test("renders event details button with the title (show details)", () => {
-    expect(EventComponent.queryByText("show details")).toBeInTheDocument();
+    expect(EventComponent.queryByText("Show Details")).toBeInTheDocument();
   });
 
   // unit test cases that check if the event’s detail’s section is shown or hidden when a user clicks on a show details button (or hide details, when the details are shown)
 
   test("by default, event's details section should be hidden", () => {
-    const hideDetailsButton = EventComponent.queryByText("hide details");
+    const hideDetailsButton = EventComponent.queryByText("Hide Details");
     expect(hideDetailsButton).not.toBeInTheDocument();
   });
 
   test("shows the details section when the user clicks on the 'show details' button", async () => {
     const user = userEvent.setup();
-    const showDetailsButton = EventComponent.queryByText("show details");
-    const detailsSection = EventComponent.queryByRole("listitem");
+    const button = EventComponent.queryByText("Show Details");
+    await user.click(button);
 
-    //    Verify that the details section is initially hidden
-    expect(detailsSection).toHaveClass("details-hidden");
-    expect(showDetailsButton).toBeInTheDocument();
-
-    await user.click(showDetailsButton);
-    const updatedDetailsSection = EventComponent.queryByRole("listitem");
-    // expect(updatedDetailsSection).toBeDefined();
-    // expect(updatedDetailsSection).toBeInTheDocument();
-    expect(updatedDetailsSection).toHaveClass("details-shown");
-    expect(updatedDetailsSection).toBeVisible();
+    const eventDOM = EventComponent.container.firstChild;
+    const details = eventDOM.querySelector(".details");
+    expect(details).toBeInTheDocument();
   });
 
   test("hides the details section when the user clicks on the 'hide details' button", async () => {
-    const user = userEvent.setup();
-    const hideDetailsButton = EventComponent.queryByText("hide details");
-    const detailsSection = EventComponent.queryByRole("listitem");
-    await user.click(hideDetailsButton);
-    EventComponent.rerender(<Event event={event} />);
-    const updatedDetailsSection = EventComponent.queryByRole("listitem");
-    expect(updatedDetailsSection).toHaveClass("details-hidden");
+    const button = EventComponent.queryByText("Show Details");
+    const eventDOM = EventComponent.container.firstChild;
+    await userEvent.click(button);
+
+    const hideButton = EventComponent.queryByText("Hide Details");
+    await userEvent.click(hideButton);
+
+    const details = eventDOM.querySelector(".details");
+    expect(details).not.toBeInTheDocument();
   });
 });
--- src/components/Event.js
+++ src/components/Event.js
@@ -2,32 +2,57 @@ import React from "react";
 import { useState } from "react";
 
 const Event = ({ event }) => {
-  const [detailsVisible, setDetailsVisible] = useState(false);
+  // const [detailsVisible, setDetailsVisible] = useState(false);
 
-  const toggleDetails = () => {
-    setDetailsVisible(!detailsVisible);
-  };
+  // const toggleDetails = () => {
+  //   setDetailsVisible(!detailsVisible);
+  // };
+
+  const [showDetails, setShowDetails] = useState(false);
 
   return (
-    <div>
-      <button onClick={toggleDetails}>
-        {detailsVisible ? "hide details" : "show details"}
-      </button>
-      {/* <button>Hide Details</button>
-      <button>Show Details</button> */}
-      <li
-        className={`show-details ${
-          detailsVisible ? "details-shown" : "details-hidden"
-        }`}
+    <li className="event">
+      <h3>{event.summary}</h3>
+      <p>{event.location}</p>
+      <p>{event.start.dateTime}</p>
+      <button
+        className="details-btn"
+        onClick={() => {
+          setShowDetails(!showDetails);
+        }}
       >
-        <h3>{event.summary}</h3>
-        <p>{event.location}</p>
-        <p>{event.start.dateTime}</p>
-        <p>{event.description}</p>
-        <p>{event.created}</p>
-      </li>
-    </div>
+        {showDetails ? "Hide Details" : "Show Details"}
+      </button>
+      {showDetails ? (
+        <div className="details">
+          <h4>Event Details</h4>
+          <p>Description: {event.description}</p>
+          <p>Event created: {event.created}</p>
+        </div>
+      ) : null}
+    </li>
   );
 };
+
+//      <div>
+//      <button onClick={toggleDetails}>
+//       {detailsVisible ? "Hide Details" : "Show Details"}
+//     </button>
+//   {/* <button>Hide Details</button>
+//       <button>Show Details</button> */}
+//     <li
+//         className={`show-details ${
+//           detailsVisible ? "details-shown" : "details-hidden"
+//         }`}
+//       >
+//         <h3>{event.summary}</h3>
+//         <p>{event.location}</p>
+//         <p>{event.start.dateTime}</p>
+//         <p>{event.description}</p>
+//         <p>{event.created}</p>
+//       </li>
+//     </div>
+//   );
+// };
 
 export default Event;
