d3cbbbc2898dd86bc6a6ec94ef5e7b79d621ed18
Ilse Gaertner <joachimpruegl@gmail.com>
2023-11-16 19:28:19 +0100

    Add SpecifyNumberOfEvent.test.js / Refactor showHideAnEventsDetails.test.js

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -8,50 +8,28 @@ import userEvent from "@testing-library/user-event";
 const feature = loadFeature("./src/features/showHideAnEventsDetails.feature");
 
 defineFeature(feature, (test) => {
-  let AppComponent;
-  let AppDOM;
-  let EventListDOM;
-  beforeEach(async () => {
-    AppComponent = render(<App />);
-    AppDOM = AppComponent.container.firstChild;
-    EventListDOM = AppDOM.querySelector("#event-list");
-  });
-
   test("Event element is collapsed by default.", ({ given, when, then }) => {
-    // let AppComponent;
-    // let AppDOM;
-
     given("the user is browsing events in a city", async () => {
-      // AppComponent = render(<App />);
-      // AppDOM = AppComponent.container.firstChild;
-      // const EventListDOM = AppDOM.querySelector("#event-list");
-      await waitFor(() => {
-        const EventListItems = within(EventListDOM).queryAllByRole("listitem");
-      });
+      const AppComponent = render(<App />);
     });
 
     when("the user views the events page", async () => {
-      // AppComponent = render(<App />);
-      // AppDOM = AppComponent.container.firstChild;
+      const AppComponent = render(<App />);
+      // const AppDOM = AppComponent.container.firstChild;
       // const EventListDOM = AppDOM.querySelector("#event-list");
-
-      await waitFor(() => {
-        const EventListItems = within(EventListDOM).queryAllByRole("listitem");
-        expect(EventListItems).toHaveLength(32);
-      });
+      // await waitFor(() => {
+      //   const EventListItems = within(EventListDOM).queryAllByRole("listitem");
+      // });
     });
 
     then(
       "the details of a specific event should be hidden by default.",
       async () => {
-        // AppComponent = render(<App />);
-        // AppDOM = AppComponent.container.firstChild;
-        let EventComponent;
-        let allEvents;
-        let event;
-        allEvents = await getEvents(); // Fetch the data once before running any test
-        event = allEvents[0];
-        EventComponent = render(<Event event={event} />);
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+        const allEvents = await getEvents(); // Fetch the data once before running any test
+        const event = allEvents[0];
+        const EventComponent = render(<Event event={event} />);
         const hideDetailsButton = EventComponent.queryByText("Hide Details");
         expect(hideDetailsButton).not.toBeInTheDocument();
       }
@@ -59,23 +37,13 @@ defineFeature(feature, (test) => {
   });
 
   test("User can expand event details.", ({ given, when, then }) => {
-    let AppComponent;
-    let AppDOM;
-    let EventComponent;
     given("the user is viewing the eventspage", async () => {
-      AppComponent = render(<App />);
-      AppDOM = AppComponent.container.firstChild;
-      const EventListDOM = AppDOM.querySelector("#event-list");
-
-      await waitFor(() => {
-        const EventListItems = within(EventListDOM).queryAllByRole("listitem");
-        expect(EventListItems).toHaveLength(32);
-      });
+      const AppComponent = render(<App />);
     });
 
     when("the user clicks on the detail button of an event", async () => {
-      AppComponent = render(<App />);
-      AppDOM = AppComponent.container.firstChild;
+      const AppComponent = render(<App />);
+      const AppDOM = AppComponent.container.firstChild;
       const EventListDOM = AppDOM.querySelector("#event-list");
 
       await waitFor(() => {
@@ -88,9 +56,8 @@ defineFeature(feature, (test) => {
     then(
       "the event details should be displayed around the button.",
       async () => {
-        AppComponent = render(<App />);
-        AppDOM = AppComponent.container.firstChild;
-
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
         const EventListDOM = AppDOM.querySelector("#event-list");
 
         await waitFor(() => {
@@ -108,24 +75,14 @@ defineFeature(feature, (test) => {
   });
 
   test("User can collapse event details.", ({ given, and, when, then }) => {
-    let AppComponent;
-    let AppDOM;
-    let event;
-    let allEvents;
-    let EventComponent;
-    let EventListDOM;
-
     given("the user is viewing the events page", async () => {
-      AppComponent = render(<App />);
-      AppDOM = AppComponent.container.firstChild;
-      EventListDOM = AppDOM.querySelector("#event-list");
-      allEvents = await getEvents();
+      const AppComponent = render(<App />);
     });
 
     and("some event details are expanded", async () => {
-      allEvents = await getEvents(); // Fetch the data once before running any test
-      event = allEvents[0];
-      EventComponent = render(<Event event={event} />);
+      const allEvents = await getEvents(); // Fetch the data once before running any test
+      const event = allEvents[0];
+      const EventComponent = render(<Event event={event} />);
       await waitFor(() => {
         const EventDOM = EventComponent.container.firstChild;
         const EventListItems = within(EventDOM).queryAllByRole("listitem");
@@ -137,8 +94,8 @@ defineFeature(feature, (test) => {
     });
 
     when(/^the user clicks on "(.*)" for that event$/, async (arg0) => {
-      AppComponent = render(<App />);
-      AppDOM = AppComponent.container.firstChild;
+      const AppComponent = render(<App />);
+      const AppDOM = AppComponent.container.firstChild;
       const EventListDOM = AppDOM.querySelector("#event-list");
 
       await waitFor(() => {
@@ -149,9 +106,8 @@ defineFeature(feature, (test) => {
     });
 
     then("the event details should be collapsed and hidden.", async () => {
-      AppComponent = render(<App />);
-      AppDOM = AppComponent.container.firstChild;
-
+      const AppComponent = render(<App />);
+      const AppDOM = AppComponent.container.firstChild;
       const EventListDOM = AppDOM.querySelector("#event-list");
 
       await waitFor(() => {
--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -3,30 +3,48 @@ import { getEvents } from "../api";
 import { waitFor, render, within } from "@testing-library/react";
 import App from "../App";
 import userEvent from "@testing-library/user-event";
+import NumberOfEvents from "../components/NumberOfEvents";
 
 const feature = loadFeature("./src/features/specifyNumberOfEvents.feature");
 
 defineFeature(feature, (test) => {
-  let AppComponent;
-  beforeEach(() => {
-    AppComponent = render(<App />);
-  });
-
   test("When user hasn't specified a number, 32 events are shown by default.", ({
     given,
     when,
     then,
   }) => {
+    let AppComponent;
     given(
       "the user has not specified a limit in the number of events shown",
-      () => {}
+      async () => {
+        AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+        const EventListDOM = AppDOM.querySelector("#event-list");
+        const CitySearchDOM = AppDOM.querySelector("#city-search");
+        const citySearchInput = within(CitySearchDOM).queryByRole("textbox");
+        await userEvent.clear(citySearchInput);
+      }
     );
 
-    when("the user browses through the events page", () => {});
+    when("the user browses through the events page", () => {
+      AppComponent = render(<App />);
+    });
 
     then(
       /^the page limits the amount of events visible to (\d+) events$/,
-      (arg0) => {}
+      async (arg0) => {
+        AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+        const EventListDOM = AppDOM.querySelector("#event-list");
+
+        await waitFor(() => {
+          const allEvents = getEvents();
+          const EventListItems =
+            within(EventListDOM).queryAllByRole("listitem");
+
+          expect(EventListItems).toHaveLength(32);
+        });
+      }
     );
   });
 
@@ -35,10 +53,39 @@ defineFeature(feature, (test) => {
     when,
     then,
   }) => {
-    given("the user wants to change the number of events displayed", () => {});
+    let AppComponent;
+    given(
+      "the user wants to change the number of events displayed",
+      async () => {
+        AppComponent = render(<App />);
+      }
+    );
 
-    when("the user enters a specific number in a text box", () => {});
+    when("the user enters a specific number in a text box", async () => {
+      waitFor(() => {
+        // AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+        const CitySearchDOM = AppDOM.querySelector("#city-search");
+        const citySearchInput = within(CitySearchDOM).queryByRole("textbox");
+        const EventListDOM = AppDOM.querySelector("#event-list");
+        const allEvents = getEvents();
+        const EventListItems = within(EventListDOM).queryAllByRole("listitem");
+        userEvent.clear(citySearchInput);
+        userEvent.type(citySearchInput, "16");
+      });
+    });
 
-    then("the number of displayed events gets updated", () => {});
+    then("the number of displayed events gets updated", async () => {
+      AppComponent = render(<App />);
+      const AppDOM = AppComponent.container.firstChild;
+      const CitySearchDOM = AppDOM.querySelector("#city-search");
+      const numbersInput = within(CitySearchDOM).queryByRole("textbox");
+      const NumberOfEventsComponent = render(
+        <NumberOfEvents setCurrentNOE={() => {}} />
+      );
+      userEvent.clear(numbersInput);
+      await userEvent.type(numbersInput, "{backspace}{backspace}10");
+      expect(numbersInput.value).toBe("10");
+    });
   });
 });
