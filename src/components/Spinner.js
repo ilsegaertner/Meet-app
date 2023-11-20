3e69fb64cef5a03314142f5de693ee8a2a2312ca
Ilse Gaertner <joachimpruegl@gmail.com>
2023-11-20 16:57:04 +0100

    Add position fixed on Spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,8 +2,15 @@ import React from "react";
 import { ProgressBar } from "react-loader-spinner";
 
 const Spinner = () => {
+  const spinnerStyle = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    zIndex: 9999,
+  };
   return (
-    <div className="spinner-container">
+    <div className="spinner-container" style={spinnerStyle}>
       <ProgressBar
         height="30"
         width="30"
