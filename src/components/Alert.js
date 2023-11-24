d7689ddf4b35c2614825a48054d8fb43c524beab
Ilse Gaertner <joachimpruegl@gmail.com>
2023-11-24 11:32:46 +0100

    Add class component Warning alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -48,4 +48,12 @@ class ErrorAlert extends Alert {
   }
 }
 
-export { InfoAlert, ErrorAlert };
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = "rgb(255, 0, 0)";
+    this.bgColor = "rgb(255, 204, 204)";
+  }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert };
