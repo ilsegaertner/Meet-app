86b84dc7e8abeab20296db7dc668958f3ddb57e7
Ilse Gaertner <joachimpruegl@gmail.com>
2023-11-27 10:28:19 +0100

    adjust margin-left scatterchart

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -34,7 +34,7 @@ const CityEventsChart = ({ allLocations, events }) => {
           top: 20,
           right: 20,
           bottom: 60,
-          left: 20,
+          left: -30,
         }}
       >
         <CartesianGrid />
